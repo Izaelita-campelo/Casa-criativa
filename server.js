596edd882d3477a544fdce9f8689bc7f4b2e92c3
Izaélita Campelo <67238328+Izaelita-campelo@users.noreply.github.com>
2020-07-28 14:35:44 -0300

    nodemon

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("olha querido")
+// Usei o express para criar e configurar o meu servidor
+const express = require("express")
+const server = express()
+
+//crei uma rota para o meu servidor
+//e capturo o pedido do cliente para responder
+server.get("/",function(req, res){
+    return res.send("Vc, Dona Iza é muito linda!")
+})
+
+
+//Liguei meu servidor na porta 3000
+
+
+server.listen(3000)
