2c1df9c3794d094934fc8cd48c0393dc074c9452
Izaélita Campelo <67238328+Izaelita-campelo@users.noreply.github.com>
2020-07-28 15:10:09 -0300

    todos funcionando

--- server.js
+++ server.js
@@ -2,11 +2,19 @@
 const express = require("express")
 const server = express()
 
+//confuguração de arquiavo estaticos(css, script, imagem)
+server.use(express.static("public"))
+
 //crei uma rota para o meu servidor
 //e capturo o pedido do cliente para responder
 server.get("/",function(req, res){
-    return res.send("Vc, Dona Iza é muito linda!")
+    return res.sendFile(__dirname + "/index.html")
 })
+
+server.get("/ideias",function(req, res){
+    return res.sendFile(__dirname + "/ideias.html")
+})
+
 
 
 //Liguei meu servidor na porta 3000
